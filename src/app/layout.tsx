41049b87de360852967093890fb83984e4d9da8d
Sebastian Jove <sebasestebanjove@gmail.com>
2026-07-30 20:47:08 +0200

    Microsoft Clarity: script condicional en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,21 @@ export default function RootLayout({
             </Script>
           </>
         )}
+        {process.env.NEXT_PUBLIC_CLARITY_ID && (
+          <Script
+            id="microsoft-clarity"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+              `,
+            }}
+          />
+        )}
       </head>
       <body className="min-h-full flex flex-col">{children}
       <FarmaFooter />
