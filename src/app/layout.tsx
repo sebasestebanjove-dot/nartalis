4e6f8cf776937da19ffb457ebdd88c7eac47b57b
Sebastian Jove <sebasestebanjove@gmail.com>
2026-07-30 20:29:04 +0200

    GA4: NEXT_PUBLIC_GA_ID + script gtag en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import FarmaFooter from "@/components/farma/FarmaFooter";
 
@@ -29,6 +30,22 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="ga-init" strategy="afterInteractive">
+              {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`}
+            </Script>
+          </>
+        )}
+      </head>
       <body className="min-h-full flex flex-col">{children}
       <FarmaFooter />
       </body>
